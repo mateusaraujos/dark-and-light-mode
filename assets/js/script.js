308d9ca66ee364d2a6246abeaf69db89eb40a899
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2022-08-08 17:44:22 -0300

    preparing to css

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,18 @@ const BUTTON = document.getElementById('mode-selector');
 const H1 = document.getElementById("title");
 // Selecting our body
 const BODY = document.getElementsByTagName("body")[0];
-const FOOTER = document.getElementsByClassName("developer");
+const FOOTER = document.getElementsByClassName("developer");    
 
 // Active dark mode or light mode function.
 function changeMode() {
+    changeClasses();
+}
 
+function changeClasses() {
+    BUTTON.classList.toggle("dark-mode");
+    H1.classList.toggle("dark-mode");
+    BODY.classList.toggle("dark-mode");
+    FOOTER.classList.toggle("dark-mode");
 }
 
 // Adding click event
