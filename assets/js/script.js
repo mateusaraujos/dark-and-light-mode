bba41616c07219c81c1bd594261f330307bc350d
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2022-08-08 17:39:32 -0300

    get elements and click event function

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const BUTTON = document.getElementById('mode-selector');
+const H1 = document.getElementById("title");
+// Selecting our body
+const BODY = document.getElementsByTagName("body")[0];
+const FOOTER = document.getElementsByClassName("developer");
+
+// Active dark mode or light mode function.
+function changeMode() {
+
+}
+
+// Adding click event
+BUTTON.addEventListener("click", changeMode);
