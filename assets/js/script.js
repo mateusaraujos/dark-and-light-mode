8976fb21673a03e133a0449f250f47a3462b1b81
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2022-08-08 18:09:57 -0300

    change text contains dark-mode class

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,37 @@
+const DARK_MODE_CLASS = "dark-mode";
 const BUTTON = document.getElementById('mode-selector');
 const H1 = document.getElementById("title");
-// Selecting our body
+// Selecting our body.
 const BODY = document.getElementsByTagName("body")[0];
-const FOOTER = document.getElementsByClassName("developer");    
+const FOOTER = document.getElementsByTagName("footer")[0];
 
-// Active dark mode or light mode function.
+// Calls "active dark-mode function".
 function changeMode() {
     changeClasses();
+    changeText();
 }
 
+// Active dark-mode.
 function changeClasses() {
-    BUTTON.classList.toggle("dark-mode");
-    H1.classList.toggle("dark-mode");
-    BODY.classList.toggle("dark-mode");
-    FOOTER.classList.toggle("dark-mode");
+    BUTTON.classList.toggle(DARK_MODE_CLASS);
+    H1.classList.toggle(DARK_MODE_CLASS);
+    BODY.classList.toggle(DARK_MODE_CLASS);
+    FOOTER.classList.toggle(DARK_MODE_CLASS);
 }
 
-// Adding click event
+function changeText() {
+    const lightMode = "Light Mode";
+    const darkMode = "Dark Mode";
+
+    if (BODY.classList.contains(DARK_MODE_CLASS)) {
+        BUTTON.innerHTML = lightMode;
+        H1.innerHTML = darkMode + " ON";
+        return;
+    }
+
+    BUTTON.innerHTML = darkMode;
+    H1.innerHTML = lightMode + " ON";
+}
+
+// Adding click event.
 BUTTON.addEventListener("click", changeMode);
