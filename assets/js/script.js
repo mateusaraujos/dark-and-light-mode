c3a382b616bf4d94fc3c9f78b4a3043eafc40f5a
Mateus Araújo <silva.mateusaraujo98@gmail.com>
2022-08-09 21:16:52 -0300

    fixing comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,23 @@
+// Constantes.
 const DARK_MODE_CLASS = "dark-mode";
 const BUTTON = document.getElementById('mode-selector');
 const H1 = document.getElementById("title");
-// Selecting our body.
+// Selecionando body e footer.
 const BODY = document.getElementsByTagName("body")[0];
 const FOOTER = document.getElementsByTagName("footer")[0];
 
-// Calls "active dark-mode function".
+/**
+ * Uma função que executa as funções changeClasses() e changeText(), respectivamente.
+ */
 function changeMode() {
     changeClasses();
     changeText();
 }
 
-// Active dark-mode.
+/**
+ * Uma função que altera a classe dos elementos das constantes BUTTON, H1, BODY e
+ * FOOTER para DARK_MODE_CLASS. Alternando entre Light Mode e Dark Mode.
+ */
 function changeClasses() {
     BUTTON.classList.toggle(DARK_MODE_CLASS);
     H1.classList.toggle(DARK_MODE_CLASS);
@@ -19,10 +25,15 @@ function changeClasses() {
     FOOTER.classList.toggle(DARK_MODE_CLASS);
 }
 
+/**
+ * Uma função que altera os textos dos elementos das constantes BUTTON e H1,
+ * respeitando uma condição.
+ */
 function changeText() {
     const lightMode = "Light Mode";
     const darkMode = "Dark Mode";
 
+    // Condição: Se a classe do elemento da constante BODY contiver DARK_MODE_CLASS.
     if (BODY.classList.contains(DARK_MODE_CLASS)) {
         BUTTON.innerHTML = lightMode;
         H1.innerHTML = darkMode + " ON";
@@ -33,5 +44,5 @@ function changeText() {
     H1.innerHTML = lightMode + " ON";
 }
 
-// Adding click event.
+// Evento de clique que aciona a função changeMode().
 BUTTON.addEventListener("click", changeMode);
